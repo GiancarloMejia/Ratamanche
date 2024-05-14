7c2256532ec2289f4a9cc8609f320efe73f35a0e
GiancarloMejia <135747147+GiancarloMejia@users.noreply.github.com>
2024-05-14 16:02:26 -0500

    ratajs.js

--- ratajs.js
+++ ratajs.js
@@ -4,12 +4,13 @@ document.addEventListener("DOMContentLoaded", function() {
     // Nombre del archivo de audio
     var nombreAudio = "Yallegotucarri.mp3";
 
-    // Generar botón
+    // Generar botón circular
     var boton = document.createElement("button");
     boton.textContent = "Yallegotucarri";
     boton.addEventListener("click", function() {
         reproducirSonido(nombreAudio);
     });
+    boton.classList.add("boton-yallegotucarri");
     botonesDiv.appendChild(boton);
 
     // Función para reproducir sonido
